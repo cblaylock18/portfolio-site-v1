95cd08283bb458c5caf4c1f78ea6e11c520b41fa
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2025-02-22 16:06:59 -0500

    feat: style and add content for the experience and footer sections

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
+import { useContext } from "react";
 import styles from "./Footer.module.css";
+import resume from "../../assets/Docs/ChristopherBlaylockResume.pdf";
+import { ThemeContext } from "../../Context/ThemeProvider";
+import { GithubSVG } from "../../assets/SVGs/GithubSVG";
+import { LinkedInSVG } from "../../assets/SVGs/LinkedInSVG";
 
 function Footer() {
-    return <footer className={styles.div}>I&apos;m a foot!</footer>;
+    const { darkMode } = useContext(ThemeContext);
+
+    return (
+        <footer className={`${styles.footer} ${darkMode ? styles.dark : ""}`}>
+            <img src="favicon.png" alt="site logo, CB" />
+            <div className={styles.links}>
+                <a
+                    href="https://github.com/cblaylock18"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Visit My Github <GithubSVG darkMode={darkMode}></GithubSVG>
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/christopher-w-blaylock/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Visit My LinkedIn{" "}
+                    <LinkedInSVG darkMode={darkMode}></LinkedInSVG>
+                </a>
+                <a href={resume} download>
+                    Download Resume (PDF 200kb)
+                </a>
+            </div>
+            <p className={styles.copyright}>
+                Copyright © 2025 Christopher Blaylock. All rights reserved.
+            </p>
+        </footer>
+    );
 }
 
 export { Footer };
